f2d0d55d15797e34b5b45fd07d6752d6de510344
StefanGoga <stefigoga@yahoo.com>
2022-02-27 17:50:13 +0200

    Login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="login">
-    <h1>This is a login page</h1>
-  </div>
+   <v-app id="inspire">
+      <v-content>
+         <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+               <v-flex xs12 sm8 md4>
+                  <v-card class="elevation-12">
+                     <v-toolbar dark color="primary">
+                        <v-toolbar-title>Formular autentificare</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                        <v-form>
+                           <v-text-field
+                              prepend-icon="mdi-account"
+                              name="login"
+                              label="Autentificare"
+                              type="text"
+                           ></v-text-field>
+                           <v-text-field
+                              id="password"
+                              prepend-icon="mdi-lock"
+                              name="Parola"
+                              label="Parola"
+                              type="password"
+                           ></v-text-field>
+                        </v-form>
+                     </v-card-text>
+                     <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary" to="/">Intra in cont</v-btn>
+                     </v-card-actions>
+                  </v-card>
+               </v-flex>
+            </v-layout>
+         </v-container>
+      </v-content>
+   </v-app>
 </template>
+
+<script>
+export default {
+   name: 'Login',
+   props: {
+      source: String,
+   },
+};
+</script>
+
+<style></style>
