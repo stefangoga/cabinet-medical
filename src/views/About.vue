ad2aeb4869d7139675557671ab7e696d6c6ea0b7
StefanGoga <stefigoga@yahoo.com>
2022-06-01 20:43:19 +0300

    making changes to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,42 @@
 <template>
-  <div class="about">
-    <h1>
-      Aplicatie web pentru gestionarea pacientilor intr-un cabinet medical
-    </h1>
+<div id="app">
+  <v-carousel cycle>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+    
+  </v-carousel>
+  
   </div>
+  
 </template>
+
+
+
+
+
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: require('../assets/poza.jpg'),
+          },
+          {
+            src: require('../assets/doctori.jpg'),
+          },
+          {
+            src: require('../assets/hol.jpg'),
+          },
+          
+        ],
+      }
+    },
+  }
+</script>
